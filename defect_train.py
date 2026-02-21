4715f698cd5fe3a4f5abfe91c33ac599d04c4568
shirazzyy <emma10162020@qq.com>
2026-02-21 16:07:53 +0800

    Add defect_train.py to train DefectModel

---
+++ defect_train.py
@@ -0,0 +1,7 @@
+# from defectDcgan.defect_model import DefectModel
+from defect_model import DefectModel # zyy 20250325
+
+if __name__ == '__main__':
+    defect = DefectModel()
+    defect.train()
+
